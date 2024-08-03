7bf67ddbfe5287dbcb0dc21243bd40133566e9e8
ambikaSoni <124435809+AmbikaFrontEndSoni@users.noreply.github.com>
2024-08-03 18:55:50 +0530

    Add files via upload

---
+++ portfolio/src/pages/Educations/Educations.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { MdSchool } from "react-icons/md";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+import "./Educations.css";
+const Education = () => {
+  return (
+    <>
+      <div className=" education" id="education">
+        <h2 className="col-12 mt-3 mb-1 text-center text-uppercase">
+          Education Details
+        </h2>
+        <hr />
+        <VerticalTimeline>
+          <VerticalTimelineElement
+            className="vertical-timeline-element--work"
+            contentStyle={{ background: "white", color: "black" }}
+            contentArrowStyle={{ borderRight: "7px solid  white" }}
+            date="2020 - 2022"
+            iconStyle={{ background: "rgb(245, 95, 230)", color: "#fff" }}
+            icon={<MdSchool />}
+          >
+            <h3 className="vertical-timeline-element-title">
+              MSC(Computer Science)
+            </h3>
+            <h4 className="vertical-timeline-element-subtitle">
+              SSL Jain PG College Vidisha(MP), IN
+            </h4>
+          </VerticalTimelineElement>
+          <VerticalTimelineElement
+            className="vertical-timeline-element--work"
+            contentStyle={{ background: "white", color: "black" }}
+            contentArrowStyle={{ borderRight: "7px solid  white" }}
+            date="2017 - 2020"
+            iconStyle={{ background: "rgb(245, 95, 230)", color: "#fff" }}
+            icon={<MdSchool />}
+          >
+            <h3 className="vertical-timeline-element-title">
+              BSC (Computer Science)
+            </h3>
+            <h4 className="vertical-timeline-element-subtitle">
+              St. Mary's PG College Vidisha(MP), IN
+            </h4>
+          </VerticalTimelineElement>
+        </VerticalTimeline>
+      </div>
+    </>
+  );
+};
+
+export default Education;
